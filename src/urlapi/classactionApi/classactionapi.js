841e9ac5f4b319e453ac656dce2e90a5b72bf46b
cangseng <1121090437@qq.com>
2019-08-26 16:51:16 +0800

    补推

--- src/urlapi/classactionApi/classactionapi.js
+++ src/urlapi/classactionApi/classactionapi.js
@@ -1,8 +1,11 @@
 const CLASSACTION_LEFTDATA = "/api/classaction_leftdata";
 const CLASSACTION_RIGHTDATA = "/api/classaction_rightdata";
 const CLASSACTION_BANNERLISTDATA = "/api/classaction_bannerlistdata";
+const CLASSACTION_NAVURLARR = ["/topic/v1 / find / recManual.json",
+    "/topic/v1/ding/sinkList.json?id=&_=1564968481825"]
 export {
     CLASSACTION_LEFTDATA,
     CLASSACTION_RIGHTDATA,
-    CLASSACTION_BANNERLISTDATA
+    CLASSACTION_BANNERLISTDATA,
+    CLASSACTION_NAVURLARR
 }
